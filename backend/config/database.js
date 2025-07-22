f003c652ead5b7613f15c862ba5bbe6568339082
Okile M Ebokorait <okile.webdev@gmail.com>
2025-07-22 23:01:29 +0200

    Sprint1_22_07_25 backend setup and merge latest main changes

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('DB Connected') //* if promise is fulfilled
+    } catch (err) {
+       console.error('DB Connection Error;', err); 
+       process.exit(1);  
+    }
+};
+export default connectDB;

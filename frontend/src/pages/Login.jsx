d2f45651d82b6667af3e4f028b6e3a86ade37d06
Chinedu <chimzykirian@gmail.com>
2025-07-24 18:24:21 +0200

    update3

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+// import { useAuth } from "../context/AuthContext";
 
 function Login() {
   const { login } = useAuth();

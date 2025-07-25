d4407055d9da1972eab6dc7b0b4583f8709fdb89
Chinedu <chimzykirian@gmail.com>
2025-07-25 11:03:05 +0200

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
->>>>>>> b736577b4134035c04c8d91b593288f8c6b9609b
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-<<<<<<< HEAD
   </StrictMode>
 );
-=======
-  </StrictMode>,
-)
->>>>>>> b736577b4134035c04c8d91b593288f8c6b9609b

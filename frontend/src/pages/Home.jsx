d5de58c633742b9d25c4ed5c856a94cc4102f61f
Student <saveenyadav28@gmail.com>
2025-08-19 13:23:00 +0200

    updated

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../components/Hero";
+import MainSection from "../components/MainSection";
+
+function Home() {
+  return (
+    <div>
+      {/* Hero Video Section */}
+      <Hero />
+
+      {/* Main Content Section */}
+      <MainSection />
+    </div>
+  );
+}
+
+export default Home;
